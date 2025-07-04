df1903354c20c2dc493626a1f8b9a11f756d178e
Mr. Will <mr.will.com@outlook.com>
2025-07-04 20:33:29 +0800

    Add Rainbow Banner page

--- app/config/_meta.js
+++ app/config/_meta.js
@@ -1,5 +1,9 @@
 export default {
   overview: 'Overview',
+  _: {
+    type: 'separator',
+    title: 'Basics',
+  },
   metadata: 'Metadata',
   hero: 'Hero Section',
   nav: 'Navigation',
@@ -7,4 +11,9 @@ export default {
   posts: 'Posts',
   'color-scheme': 'Color Scheme',
   toc: 'Table of Contents',
+  _2: {
+    type: 'separator',
+    title: 'Advanced',
+  },
+  'rainbow-banner': 'Rainbow Banner',
 }
