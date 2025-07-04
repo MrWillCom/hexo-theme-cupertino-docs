336702a8e8db2ce4cd1e62bdd3405eedde119887
Mr. Will <mr.will.com@outlook.com>
2025-07-04 18:16:36 +0800

    Update many pages

--- app/config/_meta.js
+++ app/config/_meta.js
@@ -1,4 +1,10 @@
 export default {
+  overview: 'Overview',
   metadata: 'Metadata',
   hero: 'Hero Section',
+  nav: 'Navigation',
+  footer: 'Footer',
+  posts: 'Posts',
+  'color-scheme': 'Color Scheme',
+  toc: 'Table of Contents',
 }
