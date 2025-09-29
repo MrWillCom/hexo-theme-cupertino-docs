4a7812e4b74e65dc6ba58dec9c0c94f97dcc0f08
Mr. Will <mr.will.com@outlook.com>
2025-09-29 20:12:46 +0800

    Add built-in components guide

--- app/guides/_meta.js
+++ app/guides/_meta.js
@@ -2,4 +2,5 @@ export default {
   'migrate-from-v1': 'Migrate from v1',
   'set-up-tags-and-categories-page': 'Set Up Tags & Categories Page',
   'set-up-search': 'Set Up Search',
+  'use-builtin-components': 'Use Built-in Components',
 }
